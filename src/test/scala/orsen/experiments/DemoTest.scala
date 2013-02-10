ba0d19fbbec38d177157bf21edc9ab8fbdd0020c
David Mah <Mahhaha@gmail.com>
2013-02-10 15:35:25 -0800

    Example Test Exists

---
+++ src/test/scala/orsen/experiments/DemoTest.scala
@@ -0,0 +1,16 @@
+package test.orsen.experiments
+
+import org.scalatest.FunSuite
+
+class DemoTest extends FunSuite {
+  test("5 is equal to 5") {
+    assert(5 === 5)
+  }
+
+  test("5 is equal to 6") {
+    assert(9 === 6)
+  }
+}
+
+
+// vim: set ts=4 sw=4 et:
