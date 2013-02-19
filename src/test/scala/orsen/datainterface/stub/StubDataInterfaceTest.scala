ec94c7c63fed2e85a9b69c345cf8b19ab9ef0250
David Mah <MahHaha@gmail.com>
2013-02-19 13:57:03 -0800

    Tests for StubDataInterface built out for terms
    - Also renamed to include function names

--- src/test/scala/orsen/datainterface/stub/StubDataInterfaceTest.scala
+++ src/test/scala/orsen/datainterface/stub/StubDataInterfaceTest.scala
@@ -11,27 +11,27 @@ class StubDatInterfaceTest extends FunSuite with BeforeAndAfter {
    * Sentences *
    *************/
 
-  test("The sentences iterator returns Sentence objects") {
+  test("getSentences returns an iterator of Term objects") {
     var it = StubDataInterface.getSentences()
     while (it.hasNext) {
       assert(it.next.isInstanceOf[Sentence])
     }
   }
 
-  test("Retrieving a sentence by invalid ID throws NoSuchElementException") {
+  test("getSentenceById throws NoSuchElementException with invalid id") {
     intercept[NoSuchElementException] {
       StubDataInterface.getSentenceById(-1)
       fail()
     }
   }
 
-  test("Retrieve the lowest id sentence works") {
+  test("getSentenceById retrieving lowest id works") {
     var sent = StubDataInterface.getSentenceById(4787)
     assert(sent.isInstanceOf[Sentence])
     assert(sent.id == 4787)
   }
 
-  test("Retrieve the highest id sentence works") {
+  test("getSentenceById retrieving highest id works") {
     var sent = StubDataInterface.getSentenceById(4796)
     assert(sent.isInstanceOf[Sentence])
     assert(sent.id == 4796)
@@ -41,28 +41,28 @@ class StubDatInterfaceTest extends FunSuite with BeforeAndAfter {
    * Entities *
    ************/
 
-   test("The entities iterator returns Entity objects") {
-      var it = StubDataInterface.getEntities()
-      while (it.hasNext) {
-        assert(it.next.isInstanceOf[Entity])
-      }
-   }
+  test("getEntities returns an iterator of Term objects") {
+    var it = StubDataInterface.getEntities()
+    while (it.hasNext) {
+      assert(it.next.isInstanceOf[Entity])
+    }
+  }
 
-  test("Retrieving a entity by invalid ID throws NoSuchElementException") {
+  test("getEntityById throws NoSuchElementException with invalid id") {
     intercept[NoSuchElementException] {
       StubDataInterface.getEntityById(0)
       fail()
     }
   }
 
-  test("Retrieving the lowest id entity works") {
+  test("getEntityById retrieving the lowest id entity works") {
     var ent = StubDataInterface.getEntityById(1)
     assert(ent.isInstanceOf[Entity])
     assert(ent.id == 1)
     assert(ent.name == "Luanda")
   }
 
-  test("Retrieving the highest id entity works") {
+  test("getEntityById retrieving the highest id entity works") {
     var ent = StubDataInterface.getEntityById(3)
     assert(ent.isInstanceOf[Entity])
     assert(ent.id == 3)
@@ -74,11 +74,30 @@ class StubDatInterfaceTest extends FunSuite with BeforeAndAfter {
    * Terms *
    *********/
 
-   test("The terms iterator returns Term objects") {
-      var it = StubDataInterface.getTerms
-      while (it.hasNext) {
-        assert(it.next.isInstanceOf[Term])
-      }
-   }
+  test("getTerms returns an iterator of Term objects") {
+    var it = StubDataInterface.getTerms
+    while (it.hasNext) {
+      assert(it.next.isInstanceOf[Term])
+    }
+  }
+
+  test("getTermsOfSentence throws NoSuchElementException with invalid id") {
+    intercept[NoSuchElementException] {
+       StubDataInterface.getTermsOfSentence(-1)
+    }
+  }
+
+  test("getTermById throws NoSuchElementException with invalid id") {
+    intercept[NoSuchElementException] {
+       StubDataInterface.getTermById(-1)
+    }
+  }
+
+  test("getTermById retrieving lowest id works") {
+    var term = StubDataInterface.getTermById(0)
+    assert(term.id == 0)
+    assert(term.name == "LUANDA")
+    assert(term.sentenceId == 4787)
+  }
 
 }
