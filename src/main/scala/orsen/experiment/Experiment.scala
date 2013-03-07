65691cc5b90100a4bf7f7be37440590a9384db1e
David Mah <MahHaha@gmail.com>
2013-03-07 07:17:32 -0800

    Experiment findMentions has ordering

--- src/main/scala/orsen/experiment/Experiment.scala
+++ src/main/scala/orsen/experiment/Experiment.scala
@@ -1,6 +1,7 @@
 package orsen.experiment
 
 import orsen.models._
+import scala.collection.mutable
 
 /* Runs an experiment over some Detector Output Data
  *
@@ -8,6 +9,8 @@ import orsen.models._
 object Experiment {
   def main(arguments: Array[String]) {
     // TODO Read all real mention=>entity matches
+    var goldStandard = gatherGoldStandard()
+    var matches = findMentions(goldStandard, getCandidateIterator())
     // TODO Read Data, searching for said mentions
     // TODO For each mention, build up the list of candidates
     // TODO How often is the real entity inside of the list of candidates?
@@ -17,9 +20,46 @@ object Experiment {
     // TODO: Implement
     return Map[Mention, Entity]()
   }
+  
+  def getCandidateIterator(): Iterator[(Mention, (Entity, Double))] = {
+    // TODO: Implement
+    return Array[(Mention, (Entity, Double))]().iterator
+  }
 
-  def findMentions(targetMentions: Map[Mention, Entity], 
-                   mentions: Iterator[Mention]): Map[Mention, Array[Entity]] = {
-    return Map[Mention, Array[Entity]]()
+  def createRatings(goldStandard: Map[Mention, Entity],
+                    computedLinks: mutable.Map[Mention, mutable.ArrayBuffer[Entity]]): Map[Mention, Int] = {
+    return Map[Mention, Int]()
+  }
+
+  /* Runs through mention/(candidate/probability) pairs, looking for any that have one of the target mentions
+   */
+  def findMentions(goldStandard: Map[Mention, Entity],
+                   candidateIterator: Iterator[(Mention, (Entity, Double))]
+                   ): mutable.Map[Mention, mutable.ArrayBuffer[(Entity, Double)]] = {
+    var results = mutable.Map[Mention, mutable.ArrayBuffer[(Entity, Double)]]()
+    candidateIterator.foreach {
+      (pair) => 
+      val mention = pair._1
+      val entity  = pair._2._1
+      val probability = pair._2._2
+      if (goldStandard.contains(mention)) {
+        var entityArr:mutable.ArrayBuffer[(Entity, Double)] =
+          (results.get(mention) getOrElse mutable.ArrayBuffer[(Entity, Double)]())
+        entityArr += ((entity, probability))
+        println(entityArr)
+        results.put(mention, entityArr)
+      }
+    }
+// sort by 2nd element
+// Sorting.quickSort(pairs)(Ordering.by[(String, Int, Int), Int](_._2)
+
+    results.foreach {
+      (candidateList) =>
+      var mention = candidateList._1
+      var entityArr = candidateList._2
+      
+      results.put(mention, entityArr.sortWith(_._2>_._2))
+    }
+    return results
   }
 }
--- src/test/scala/orsen/experiment/ExperimentTest.scala
+++ src/test/scala/orsen/experiment/ExperimentTest.scala
@@ -4,8 +4,89 @@ import org.scalatest.FunSuite
 import orsen.experiment._
 import orsen.models._
 
+import scala.collection.mutable
+
 class ExperimentTests extends FunSuite {
+
   test("gatherGoldStandard should retrieve true entities") {
     fail("This probably won't ever get tested! Ad Hoc it!")
   }
+
+  val mentions = Array(new Mention(1, "asd", 5),
+                       new Mention(2, "bsd", 5),
+                       new Mention(3, "tsd", 5))
+
+  val entities = Array(new Entity(101, "asd"),
+                       new Entity(102, "bsd"),
+                       new Entity(103, "tsd"),
+                       new Entity(104, "tsd"))
+
+
+  val goldStandard = Map[Mention, Entity](
+                        mentions(0)->entities(0),
+                        mentions(1)->entities(1),
+                        mentions(2)->entities(2))
+
+  val candidates  = Array(((entities(1), 100.0)),
+                          ((entities(2), 75.0)),
+                          ((entities(3), 25.0)))
+
+  val computedLinks = Map[Mention, mutable.ArrayBuffer[(Entity, Double)]](
+                        mentions(1)->mutable.ArrayBuffer((entities(1),100.0)),
+                        mentions(2)->mutable.ArrayBuffer((entities(2),75.0),
+                                                         (entities(3),25.0))
+                      )
+
+  val ratings = Map[Mention, Int]( // TODO INCOMPLETE
+                  mentions(0)->(-1),
+                  mentions(1)->0,
+                  mentions(2)->1
+                )
+
+
+  test("findMentions works in the common cases") {
+
+    var matches = Array(mentions(1)->candidates(0), // One candidate
+                        new Mention(3, "tsd", 5)->candidates(1), // Multiple candidates
+                        new Mention(3, "tsd", 5)->candidates(2),// Multiple candidates
+                        new Mention(4, "led", 6)->candidates(0) // Not a target
+                         )
+    // Handle both same reference and different (equivalent) instance case
+    // Handle both single entity and multiple entity cases
+    var expected = computedLinks
+
+    // From one ordering of entities
+    var results  = Experiment.findMentions(goldStandard, matches.iterator)
+    assert(expected.size === results.size)
+    results.foreach {
+      (keyValue) =>
+      assert(expected(keyValue._1) === results(keyValue._1))
+    }
+  }
+
+  test("findMentions works if you switch up input order ") {
+
+    // Handle both same reference and different (equivalent) instance case
+    // Handle both single entity and multiple entity cases
+    var matches = Array(mentions(1)->candidates(0), // One candidate
+                        new Mention(3, "tsd", 5)->candidates(2),// Multiple candidates
+                        new Mention(3, "tsd", 5)->candidates(1), // Multiple candidates
+                        new Mention(4, "led", 6)->candidates(0) // Not a target
+                         )
+    var expected = computedLinks
+
+    // From one ordering of entities
+    var results  = Experiment.findMentions(goldStandard, matches.iterator)
+    assert(expected.size === results.size)
+    results.foreach {
+      (keyValue) =>
+      assert(expected(keyValue._1) === results(keyValue._1))
+    }
+  }
+
+  test("createRatings works in the common cases") {
+    // Experiment.createRatings(goldStandard, computedLinks)
+  }
+
+
 }
